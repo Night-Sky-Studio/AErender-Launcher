de22cb0d390b71ce4e2c5aab12b5e9ca86605c25
Lily Stilson <lunamusicoff@gmail.com>
2022-06-17 18:18:53 +0200

    Inital commit

---
+++ src/AErender Launcher/AErenderLauncher/Views/MainView.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace AErenderLauncher.Views {
+    public partial class MainView : UserControl {
+        public MainView() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/AErender Launcher/AErenderLauncher/Views/MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace AErenderLauncher.Views {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
