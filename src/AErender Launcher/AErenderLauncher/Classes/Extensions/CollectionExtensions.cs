f945955e3614e4b3458cce6d787e9287dde9dbbb
LilyStilson <lunamusicoff@gmail.com>
2024-02-10 17:18:50 +0100

    feat: RemoveAll and TryCast extensions

--- src/AErender Launcher/AErenderLauncher/Classes/Extensions/CollectionExtensions.cs
+++ src/AErender Launcher/AErenderLauncher/Classes/Extensions/CollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using AErenderLauncher.Classes.Rendering;
@@ -27,4 +28,24 @@ public static class CollectionExtensions {
     public static RenderTask GetTaskById(this IEnumerable<RenderTask> tasks, int id) => tasks.First(x => x.ID == id);
     
     public static Composition GetCompositionByName(this IEnumerable<Composition> compositions, string name) => compositions.First(x => x.CompositionName == name);
+    
+    public static IList<T> RemoveAll<T>(this IList<T> list, Predicate<T> match) {
+        for (int i = 0; i < list.Count; i++) {
+            if (!match(list[i])) continue;
+            list.RemoveAt(i);
+            i--;
+        }
+
+        return list;
+    }
+    
+    public static bool TryCast<T>(this IList list, out IList<T>? result) {
+        try {
+            result = list.Cast<T>().ToList();
+            return true;
+        } catch {
+            result = null;
+            return false;
+        }
+    }
 }
