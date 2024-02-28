566843e74bd722950833c247e9ae096786399658
LilyStilson <lunamusicoff@gmail.com>
2024-02-28 18:58:14 +0100

    test: coverage improve

--- src/AErenderLauncher.Test/AErenderParsingTest.cs
+++ src/AErenderLauncher.Test/AErenderParsingTest.cs
@@ -6,24 +6,33 @@ namespace AErenderLauncher.Test;
 
 [TestFixture]
 public class AErenderParsingTest {
-    const string DurationData = "PROGRESS:  Duration: 0:00:10:00";
-    const string FrameRateData = "PROGRESS:  Frame Rate: 23.976 (comp)";
-    const string FrameData = "PROGRESS:  0:00:00:00 (1): 0 Seconds";
-    
     [Test]
     public void TestParseDuration() {
         Timecode correct = new(0, 0, 10, 0);
-        Timecode? duration = AerenderParser.ParseDuration(DurationData);
+        Timecode? duration = AerenderParser.ParseDuration("PROGRESS:  Duration: 0:00:10:00");
         Assert.That(duration != null);
         Assert.That(duration.ToString() == correct.ToString());
+        Assert.That(duration?.ToFrames(60) == 600);
+    }
+
+    [Test]
+    public void TestParseDurationError() {
+        Timecode? duration = AerenderParser.ParseDuration("PROGRESS: Duration: a:10:00");
+        Assert.That(duration == null);
     }
     
     [Test]
     public void TestParseFrameRate() {
         double correct = 23.976;
-        double? framerate = AerenderParser.ParseFramerate(FrameRateData);
+        double? framerate = AerenderParser.ParseFramerate("PROGRESS:  Frame Rate: 23.976 (comp)");
         Assert.That(framerate != null);
         Assert.That(Math.Abs((double)framerate! - correct) < 0.00000001);
+    }
+    
+    [Test]
+    public void TestParseFrameRateError() {
+        double? framerate = AerenderParser.ParseFramerate("PROGRESS: Frame Rate: 23.");
+        Assert.That(framerate == null);
     }
     
     [Test]
@@ -33,8 +42,17 @@ public class AErenderParsingTest {
             Frame = 1,
             ElapsedTime = 0
         };
-        AerenderFrameData? frameData = AerenderParser.ParseFrameData(FrameData);
+        AerenderFrameData? frameData = AerenderParser.ParseFrameData("PROGRESS:  0:00:00:00 (1): 0 Seconds");
         Assert.That(frameData != null);
         Assert.That(frameData.ToString() == correct.ToString());
+    }
+    
+    [Test]
+    public void TestParseFrameDataError() {
+        AerenderFrameData? frameData = AerenderParser.ParseFrameData("PROGRESS:  0:00 (22.3): 5 Seconds");
+        Assert.That(frameData == null);
+        
+        frameData = AerenderParser.ParseFrameData(" 0:00 (22.3): 5 Seconds");
+        Assert.That(frameData == null);
     }
 }
--- src/AErenderLauncher.Test/RenderTaskTests.cs
+++ src/AErenderLauncher.Test/RenderTaskTests.cs
@@ -11,6 +11,9 @@ public class RenderTaskTests {
     public void TestFrameSpan() {
         FrameSpan span = new (0, 59);
         Assert.That(span.ToString() == "[0, 59]");
+        
+        span = new ("0", "59");
+        Assert.That(span.ToString("({0}..{1})") == "(0..59)");
     }
 
     [Test]
@@ -25,6 +28,12 @@ public class RenderTaskTests {
     public void TestComposition() {
         Composition comp = new ("Game Icons", new FrameSpan(0, 599), 1);
         Assert.That(comp.ToString() == "Comp(Game Icons, [0, 599], 1)");
+
+        Composition newComp = comp.Clone();
+        Assert.That(newComp.ToString() == "Comp(Game Icons, [0, 599], 1)");
+
+        comp = new();
+        Assert.That(comp.ToString() == "Comp(, [0, 0], 1)");
     }
 
     [Test]
@@ -32,12 +41,17 @@ public class RenderTaskTests {
         Composition comp = new ("Game Icons", new FrameSpan(0, 599), 4);
         
         Assert.That(comp.SplitFrameSpans.AsString() == "[0, 149]\n[150, 298]\n[299, 447]\n[448, 599]\n");
+        
+        comp.Split = 2;
+        
+        Assert.That(comp.SplitFrameSpans.AsString() == "[0, 299]\n[300, 599]\n");
     }
     
     // RenderTask tests
     [Test]
     public void TestRenderTask() {
-        RenderTask task = new RenderTask {
+        string taskStr = "(C:\\YandexDisk\\Acer\\Footages (AE)\\AErender Launcher Benchmark Projects\\Deneb - Mograph Icons\\Mograph Icons.aep, C:\\Users\\lunam\\Desktop\\[projectName]\\[compName].[fileExtension], Lossless, Best settings, True, True, False, -reuse, 100, 5, [Comp(Game Icons, [0, 599], 1), Comp(Web Icons, [0, 599], 1), Comp(Ecology Icons, [0, 599], 1), Comp(Medical Icons, [0, 599], 1)])";
+        RenderTask task = new () {
             Project = "C:\\YandexDisk\\Acer\\Footages (AE)\\AErender Launcher Benchmark Projects\\Deneb - Mograph Icons\\Mograph Icons.aep",
             Output = "C:\\Users\\lunam\\Desktop\\[projectName]\\[compName].[fileExtension]",
             OutputModule = "Lossless",
@@ -45,6 +59,7 @@ public class RenderTaskTests {
             MissingFiles = true,
             Sound = true,
             Multiprocessing = false,
+            CustomProperties = "-reuse",
             CacheLimit = 100,
             MemoryLimit = 5,
             Compositions = [
@@ -54,7 +69,34 @@ public class RenderTaskTests {
                 new Composition("Medical Icons", new FrameSpan(0, 599), 1),
             ]
         };
-        Assert.That(task.ToString() == "(C:\\YandexDisk\\Acer\\Footages (AE)\\AErender Launcher Benchmark Projects\\Deneb - Mograph Icons\\Mograph Icons.aep, C:\\Users\\lunam\\Desktop\\[projectName]\\[compName].[fileExtension], Lossless, Best settings, True, True, False, , 100, 5, [Comp(Game Icons, [0, 599], 1), Comp(Web Icons, [0, 599], 1), Comp(Ecology Icons, [0, 599], 1), Comp(Medical Icons, [0, 599], 1)])"
-        );
+        Assert.That(task.ToString() == taskStr);
+        Assert.That(task.ID != 0);
+        Assert.That(task.ProjectName != "");
+        Assert.That(task.State == RenderTask.RenderState.Waiting);
+
+        var queue = task.Enqueue();
+        Assert.That(queue.Count == 4);
+        Assert.That(queue[0].Name == "Game Icons");
+        
+        var newTask = task.Clone();
+        Assert.That(newTask.ToString() == taskStr);
+        
+        newTask.Output = "C:\\Users\\lunam\\Desktop\\Mograph Icons\\[compName].[fileExtension]";
+        queue = newTask.Enqueue();
+        Assert.That(queue[0].FullCommand.Contains(newTask.Output));
+
+        task = new() {
+            Project = "C:\\YandexDisk\\Acer\\Footages (AE)\\AErender Launcher Benchmark Projects\\Deneb - Mograph Icons\\Mograph Icons.aep",
+            Output = "C:\\Users\\lunam\\Desktop\\[projectName]\\[projectName]\\[compName].[fileExtension]",
+            Compositions = [
+                new Composition("Game Icons", new FrameSpan(0, 0), 1),
+                new Composition("Web Icons", new FrameSpan(0, 599), 2)
+            ]
+        };
+        queue = task.Enqueue();
+        Assert.That(queue.Count == 3);
+        Assert.That(queue[0].FullCommand.Contains("-s 0 -e 0") == false);
+        Assert.That(queue[1].FullCommand.Contains("-s 0 -e 299") && queue[1].FullCommand.Contains("[compName]_0"));
+        Assert.That(Directory.Exists("C:\\Users\\lunam\\Desktop\\Mograph Icons\\Mograph Icons"));
     }
 }
