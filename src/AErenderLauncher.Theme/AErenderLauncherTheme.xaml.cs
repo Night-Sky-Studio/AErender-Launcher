82aa7d927d559b07c10e0d52c1ccfbdba0b9762c
LilyStilson <lunamusicoff@gmail.com>
2024-12-02 22:34:36 +0100

    feat: added theme files to repository

---
+++ src/AErenderLauncher.Theme/AErenderLauncherTheme.xaml.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace AErenderLauncher.Theme;
+
+public class AErenderLauncherTheme : Styles {
+    public AErenderLauncherTheme(IServiceProvider? provider = null) 
+        => AvaloniaXamlLoader.Load(provider, this);
+}
