1b8fde5847ddbf6d9515c849821e37e2fa3797b5
LilyStilson <lunamusicoff@gmail.com>
2025-01-12 23:05:43 +0100

    fix: added property changed notification attributes for dependent properties

--- src/AErenderLauncher/ViewModels/AboutWindowViewModel.cs
+++ src/AErenderLauncher/ViewModels/AboutWindowViewModel.cs
@@ -6,15 +6,15 @@ using Semver;
 namespace AErenderLauncher.ViewModels;
 
 public partial class AboutWindowViewModel : ObservableObject {
-    [ObservableProperty]
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(VersionText))]
     private SemVersion _version = App.Version;
 
     public string VersionText => Version.WithoutMetadata().ToString();
 
-    [ObservableProperty]
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(FFmpegInfo))]
     private FFmpeg? _ffmpeg = Settings.Current.FFmpeg;
 
-    [ObservableProperty]
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(AfterFxInfo))]
     private AfterFx? _afterFx = Settings.Current.AfterEffects;
 
     public string? FFmpegInfo => Ffmpeg is not null ? $"{Ffmpeg.Version} ({Ffmpeg.Path})" : null;
